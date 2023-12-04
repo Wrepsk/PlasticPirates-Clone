b8d2b2c01a236e99c0ef27966ae332e64e8bc2e1
hakan <hakancandar@proton.me>
2023-12-04 22:14:58 +0100

    implement simple hud

--- Assets/Scripts/Stats/DebugStatsOverlay.cs
+++ Assets/Scripts/Stats/DebugStatsOverlay.cs
@@ -4,13 +4,38 @@ using UnityEngine.UI;
 
 public class DebugStatsOverlay : MonoBehaviour
 {
+
+    HUD hud;
+
     Text collectedTrash;
+
+    Canvas canvas;
 
     void Start() {
         Transform panel = transform.Find("Canvas/Panel");
         collectedTrash = panel.Find("CollectedTrash/Value").gameObject.GetComponent<Text>();
 
         StatsManager.instance.PropertyChanged += OnStatsChanged;
+
+        hud = FindObjectOfType<HUD>();
+        canvas = transform.Find("Canvas").GetComponent<Canvas>();
+    }
+
+    void Update()
+    {
+        if (hud != null)
+        {
+            if (Input.GetKey(KeyCode.K))
+            {
+                canvas.enabled = true;
+                hud.gameObject.SetActive(false);
+            }
+            else
+            {
+                canvas.enabled = false;
+                hud.gameObject.SetActive(true);
+            }
+        }
     }
 
     void OnStatsChanged(object sender, PropertyChangedEventArgs propertyChange) {
